c88162a55f20718d618d9515cba458cb56d3d2ba
Julius Alvarado <angularjs.uiux@gmail.com>
2017-09-17 20:34:56 -0700

    basic post to my nosql database

--- app/states/post/ctrl.post.js
+++ app/states/post/ctrl.post.js
@@ -3,13 +3,22 @@
  */
 
 
-angular.module('edhubJobsApp').controller('PostJobCtrl', [
-    function () {
+angular.module('edhubJobsApp').controller('PostJobCtrl', ['edhubJobPostService',
+    function (edhubJobPostService) {
         const vm = this;
         vm.organization = {
-            name: '',
-            location: '',
+            organizationName: '',
+            zipCode: '',
             email: ''
+        };
+
+        vm.postJob = function () {
+
+            edhubJobPostService.jobPostings.$add(vm.organization).then(function (res) {
+                console.log("jha - successfully posted job to firebase ^_^/ res=");
+                console.log(res);
+                vm.organization = {};
+            });
         };
     }
 ]);
